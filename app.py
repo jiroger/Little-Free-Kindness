248856f7508f65d1d767481db6438a3acdbf0f78
roger ji <roger.ji.32021@gmail.com>
2019-08-08 10:51:21 -0700

    testing confiig settings

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import os
 from flask import Flask
 
 app = Flask(__name__)
-app.config.from_object(os.environ['APP_SETTINGS'])
+print(app.config.from_object(os.environ['APP_SETTINGS']))
 
 @app.route('/')
 def hello():
