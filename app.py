c95586ed0c4bb38f102a65c5548f324fba4ec19e
roger ji <roger.ji.32021@gmail.com>
2019-08-08 10:52:35 -0700

    heroku config not workiing....

--- app.py
+++ app.py
@@ -2,7 +2,8 @@ import os
 from flask import Flask
 
 app = Flask(__name__)
-print(app.config.from_object(os.environ['APP_SETTINGS']))
+app.config.from_object(os.environ['APP_SETTINGS'])
+print(os.environ['APP_SETTINGS'])
 
 @app.route('/')
 def hello():
