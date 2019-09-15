522fabd37271aeb962f55ba1346827c4be74752c
roger ji <roger.ji.32021@gmail.com>
2019-09-14 17:01:34 -0700

    navbar

--- forms.py
+++ forms.py
@@ -1,6 +1,6 @@
 from flask_wtf import FlaskForm, RecaptchaField
 from wtforms import TextField, TextAreaField, SubmitField
-from wtforms.validators import DataRequired, Email
+from wtforms.validators import *
 
 class InputForm(FlaskForm):
     name = TextField("Name")
@@ -9,7 +9,7 @@ class InputForm(FlaskForm):
     submit = SubmitField("Send")
 
 class ViewForm(FlaskForm):
-    id = TextField("Please enter your post's ID to view its statistics", validators=[DataRequired()])
+    id = TextField("Please enter your post's ID to view its statistics", validators=[DataRequired(), UUID(message="Not a valid UUID!")])
     submit = SubmitField("Lezgo!")
 
 class VoteForm(FlaskForm):
